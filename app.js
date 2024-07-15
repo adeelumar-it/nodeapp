3e1031657067c08cf04e78ec1817792c148a7317
adeelumar <146169954+adeelumar-it@users.noreply.github.com>
2024-07-14 23:10:36 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,19 +6,20 @@ const cors = require('cors');
 
 const app = express();
 const port = 8000;
-app.use(cors())
+app.use(cors());
+
 // Middleware
 app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/advocite', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('Connected to MongoDB');
-}).catch(err => {
-    console.error('MongoDB connection error:', err);
-});
+
+mongoose.connect('mongodb://adeelumar154:adeelumar(143)$@ac-xyamjwa-shard-00-00.kowlopz.mongodb.net:27017,ac-xyamjwa-shard-00-01.kowlopz.mongodb.net:27017,ac-xyamjwa-shard-00-02.kowlopz.mongodb.net:27017/advocite?ssl=true&replicaSet=atlas-eg5czy-shard-0&authSource=admin&retryWrites=true&w=majority&appName=advocite')
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(err => {
+        console.error('MongoDB connection error:', err);
+    });
 
 // Routes
 app.use('/api/data', dataRoutes);
